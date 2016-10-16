f1183f6ed1e2ad54b02f15b1cb9b1f2db1ff56e2
Nils Inge Rugsveen <nirugsveen@gmail.com>
2016-10-16 21:56:48 +0200

    Router now working

---
+++ src/components/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Test = React.createClass({
+    render() {
+        return (
+            <div>
+                <h1>Dette er en test</h1>
+            </div>
+        );
+    }
+});
+
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
+import App from './components/App.jsx';
+import Test from './components/Test.jsx';
 
 ReactDOM.render(
-    <div>Hello world!</div>,
+    <Router history={hashHistory}>
+        <Route path='/' component={App} />
+        <Route path='/test' component={Test} />
+    </Router>,
     document.getElementById('content')
 );
