389afcf007d41beb73bc8e176fe182b670417d99
Nils Inge Rugsveen <nirugsveen@gmail.com>
2016-11-08 07:03:11 +0100

    Changed to hashHistory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App from './components/App.jsx';
 import Home from './components/Home.jsx';
@@ -9,7 +9,7 @@ import Flow from './components/Campus/Flow/Flow.jsx';
 import CfP from './components/Campus/CfP/CfP.jsx';
 
 ReactDOM.render(
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
         <Route path='/' component={App} />
         <Route path='/campus' component={Campus} />
         <Route path='/campus/flow' component={Flow} />
